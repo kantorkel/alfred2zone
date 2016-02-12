8ad3dc4a8f97abd7e1373a51cb87c91d6c9e3121
kantorkel <kantorkel@users.noreply.github.com>
2016-02-12 21:26:47 +0100

    HHer foo

--- alfred2zone.py
+++ alfred2zone.py
@@ -7,20 +7,21 @@ from ipaddress import *
 from time import time
 
 ValidHostnameRegex = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
-#prefix = IPv6Network('fdef:ffc0:3dd7::/64')
-prefix = IPv6Network('2001:67c:2d50::/48')
+prefix = IPv6Network('2a03:2267::/64')
 
 data = json.load(sys.stdin)["nodes"].values()
 
 print("""$TTL 600  ; 10 minutes
-@     IN SOA  srv01.luebeck.freifunk.net. info.luebeck.freifunk.net. (
+@     IN SOA  srv01.hamburg.freifunk.net. hostmaster.hamburg.freifunk.net. (
           %i ; serial
           600        ; refresh (10min)
           30         ; retry (30s)
           3600       ; expire (1 hour)
           60         ; minimum (1 minute)
           )
-	  NS srv01.luebeck.freifunk.net.
+          NS srv01.hamburg.freifunk.net.
+          NS named.exosphere.de.
+          NS ns.ohrensessel.net.
       """ % time())
 
 HostnameRegex = re.compile(ValidHostnameRegex)
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LC_ALL=en_US.UTF-8
-curl https://map.luebeck.freifunk.net/data/nodes.json | /usr/bin/python3 /usr/local/bin/alfred2zone/alfred2zone.py > /var/cache/bind/nodes.ffhl.zone
+curl https://map.hamburg.freifunk.net/nodes.json | /usr/bin/python3 /usr/local/bin/alfred2zone/alfred2zone.py > /var/cache/bind/nodes.ffhh.zone
 /usr/sbin/rndc reload
